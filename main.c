34e12f2888bae61136b56c915a030edded14be55
hideki233 <hidekirangui@gmail.com>
2026-02-18 19:09:32 -0300

    Implemeta MMU com leitura inicial da ROM e leitura por endereco

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "rom.h"
+#include "mmu.h"
 
 int main(int argc, char *argv[]) {
+
     if (argc < 2) {
         printf("Uso: gbemu [arquivo ROM]\n");
         return 1;
@@ -13,6 +16,14 @@ int main(int argc, char *argv[]) {
         printf("Nao foi possivel ler a rom %s\n", argv[1]);
         return 1;
     }
+
+    MMU mmu;
+    mmu_init(&mmu, &rom);
+   printf("mmu criado");
+
+    uint8_t b = mmu_read8(&mmu, 0x0134);
+    printf("Primeiro byte do titulo (via mmu): 0x%02X\n", b);
+
 
     rom_print_header(&rom);
     rom_free(&rom);
